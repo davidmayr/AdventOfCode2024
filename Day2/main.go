50e6d422471e4d9842ba33ec2efd7db515b99bea
David Mayr <davidliebtkekse@gmail.com>
2024-12-02 09:50:57 +0100

    feat: task 2

--- Day2/main.go
+++ Day2/main.go
@@ -16,17 +16,13 @@ func main() {
 	}
 
 	correct := 0
+	correctTwo := 0
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		text := scanner.Text()
 		split := strings.Split(text, " ")
 		numbers := make([]int, 0, len(split))
-
-		increasing := false
-		defined := false
-		prev := 0
-		failed := false
 
 		for _, s := range split {
 			number, err := strconv.Atoi(s)
@@ -37,46 +33,79 @@ func main() {
 			numbers = append(numbers, number)
 		}
 
-		for i, number := range numbers {
-			if i > 0 && !defined {
-				if number > prev {
-					increasing = true
-				}
-				defined = true
-			}
+		increasing := isIncreasing(numbers)
+		decreasing := isDecreasing(numbers)
+		test1 := test(numbers, increasing)
 
-			var diff int
-
-			if increasing {
-				diff = number - prev
-			} else {
-				diff = prev - number
-			}
-
-			println(diff)
-			if i > 0 {
-				if increasing && number < prev {
-					println("Failed 2")
-					failed = true
-					break
-				} else if !increasing && number > prev {
-					println("Failed 1")
-					failed = true
-					break
-				} else if diff <= 0 || diff > 3 {
-					println("Failed 3")
-
-					failed = true
-					break
-				}
-			}
-
-			prev = number
-		}
-		println("___")
-		if !failed {
+		if (increasing || decreasing) && test1 {
 			correct = correct + 1
+		}
+
+		//Part two
+		for i := range len(numbers) {
+
+			listWithoutIndex := append([]int{}, numbers[:i]...)
+			listWithoutIndex = append(listWithoutIndex, numbers[i+1:]...)
+
+			increasing2 := isIncreasing(listWithoutIndex)
+			decreasing2 := isDecreasing(listWithoutIndex)
+			test2 := test(listWithoutIndex, increasing2)
+
+			if (increasing2 || decreasing2) && test2 {
+				correctTwo = correctTwo + 1
+				break
+			}
 		}
 	}
 	println(correct)
+	println(correctTwo)
+}
+
+func test(numbers []int, increasing bool) bool {
+	prev := 0
+
+	for i, number := range numbers {
+		var diff int
+
+		if increasing {
+			diff = number - prev
+		} else {
+			diff = prev - number
+		}
+
+		if i > 0 && (diff <= 0 || diff > 3) {
+			return false
+		}
+
+		prev = number
+	}
+	return true
+}
+
+func isIncreasing(numbers []int) bool {
+
+	for i := range numbers {
+		if i == len(numbers)-1 {
+			break
+		}
+
+		if numbers[i] > numbers[i+1] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func isDecreasing(numbers []int) bool {
+	for i := range numbers {
+		if i == len(numbers)-1 {
+			break
+		}
+
+		if numbers[i] < numbers[i+1] {
+			return false
+		}
+	}
+	return true
 }
